135aa584a36e8e2dd8698ad554164bc1523dc20e
Louie T <tselouie@gmail.com>
2023-02-23 14:48:46 -0500

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,94 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  // Don't retry more than 10 times
+  const maxRetries = 3;
+  const [input, setInput] = useState("");
+  const [img, setImg] = useState("");
+  const [retry, setRetry] = useState(0);
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState("");
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  const generateAction = async () => {
+    console.log("Generating...");
+
+    // Add this check to make sure there is no double click
+    if (isGenerating && retry === 0) return;
+
+    setIsGenerating(true);
+
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
+    const response = await fetch('/api/generate', {
+      method: "POST",
+      headers: {
+        "Content-Type": "image/jpeg",
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+
+    if (response.status === 503) {
+      // Set the estimated_time property in state
+      setRetry(data.estimated_time);
+      return;
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      return;
+    }
+
+    // Set image data into state property
+    setFinalPrompt(input);
+    setInput("");
+    setImg(data.image);
+    setIsGenerating(false);
+  };
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(
+          `Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`
+        );
+        setRetryCount(maxRetries);
+        return;
+      }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
+
   return (
     <div className="root">
       <Head>
@@ -11,24 +97,38 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Image Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Generate Image using Louie as the Model!</h2>
+          </div>
+          <div className="prompt-container">
+            {/* Add value property */}
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className="prompt-buttons">
+              <a
+                className={
+                  isGenerating ? "generate-button loading" : "generate-button"
+                }
+                onClick={generateAction}
+              >
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={input} />
+            <p>{finalPrompt}</p>
           </div>
-        </a>
+        )}
       </div>
     </div>
   );
